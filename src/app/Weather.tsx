5583bd7d048ebef67333744962529659b53fba34
ian <icpmacdo@gmail.com>
2023-06-17 20:47:30 -0700

    updated styling

--- src/app/Weather.tsx
+++ src/app/Weather.tsx
@@ -24,29 +24,35 @@ const Weather: React.FC = () => {
 
   return (
     <div className="max-w-md mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
-        <div>
-          <h2 className="text-2xl font-bold mb-2">Weather in: </h2>
-          <input
-            id="searchInput"
-            type="text"
-            placeholder="Search..."
-            value={searchInput}
-            onChange={handleInputChange}
-          />
-
-          <button onClick={fetchWeatherData}>Search</button>
-
-          {/* Render weather data */}
-          {weatherData && (
-            <div>
-              {/* Display weather information */}
-              {/* Example: */}
-              <p className="text-lg">{Math.round(weatherData.main.temp - 273.15)}°C</p>
-              <p className="text-lg">{weatherData.weather[0].description}</p>
-            </div>
-          )}
-        </div>
+    <div>
+      <h2 className="text-2xl font-bold mb-2">Weather in:</h2>
+      <input
+        id="searchInput"
+        type="text"
+        placeholder="Search..."
+        value={searchInput}
+        onChange={handleInputChange}
+        className="mb-4 w-full p-2 border-2 border-gray-200 rounded shadow-sm"
+      />
+    
+      <div className="flex justify-center">
+        <button 
+          className="text-lg border-2 border-blue-500 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 mb-4" 
+          onClick={fetchWeatherData}>
+          Search
+        </button>
+      </div>
+      {/* Render weather data */}
+      {weatherData && (
+        <div className="p-4 bg-blue-100 rounded-lg shadow-inner text-center">
+            {/* Display weather information */}
+            {/* Example: */}
+            <p className="text-2xl font-bold text-blue-800">{Math.round(weatherData.main.temp - 273.15)}°C</p>
+            <p className="text-xl text-gray-700 capitalize">{weatherData.weather[0].description}</p>
+        </div>    
+      )}
     </div>
+  </div>
   );
 };
 
