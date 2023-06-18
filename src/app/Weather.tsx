e41b71e9a45cbb503ca0c2ef2b5272719ae144b9
ian <icpmacdo@gmail.com>
2023-06-17 18:56:54 -0700

    Updated weather component

--- src/app/Weather.tsx
+++ src/app/Weather.tsx
@@ -1,37 +1,51 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Weather: React.FC = () => {
   const [weatherData, setWeatherData] = useState<any>(null);
+  const [searchInput, setSearchInput] = useState<any>('');
 
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=ottawa&appid=${apiKey}`;
-      try {
-        const response = await axios.get(url);
-        setWeatherData(response.data);
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-      }
-    };
+  const fetchWeatherData = async () => {
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}`;
+    try {
+      console.log(url)
+      const response = await axios.get(url);
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
 
-    fetchWeatherData();
-  }, []);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+  };
 
   return (
     <div className="max-w-md mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
-      {weatherData ? (
         <div>
-          <h2 className="text-2xl font-bold mb-2">Weather in {weatherData.name}</h2>
-          <p className="text-lg text-blue-600">Temperature: {Math.round(weatherData.main.temp - 273.15)}°C</p>
-          <p className="text-lg">Description: {weatherData.weather[0].description}</p>
-          {/* Additional weather information */}
+          <h2 className="text-2xl font-bold mb-2">Weather in: </h2>
+          <input
+            id="searchInput"
+            type="text"
+            placeholder="Search..."
+            value={searchInput}
+            onChange={handleInputChange}
+          />
+
+          <button onClick={fetchWeatherData}>Search</button>
+
+          {/* Render weather data */}
+          {weatherData && (
+            <div>
+              {/* Display weather information */}
+              {/* Example: */}
+              <p className="text-lg">{Math.round(weatherData.main.temp - 273.15)}°C</p>
+              <p className="text-lg">{weatherData.weather[0].description}</p>
+            </div>
+          )}
         </div>
-      ) : (
-        <p>Loading weather data...</p>
-      )}
     </div>
   );
 };
